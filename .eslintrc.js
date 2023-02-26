5124277de08d1125d846a8f2c1401ca180a9f790
yandexlispeth <malinamagic@yandex.ru>
2023-02-27 01:17:01 +0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+export const config = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "@typescript-eslint/no-empty-function": 0,
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/no-non-null-assertion": 0,
+        "@typescript-eslint/no-this-alias": 0,
+    }
+}
