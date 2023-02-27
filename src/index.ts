7f24ef15c923b0c20cfacbcab175866f92662347
yandexlispeth <malinamagic@yandex.ru>
2023-02-28 02:15:46 +0300

    Правки по компонентам

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,57 @@
-// import template from './template.hbs';
+import template from './index.hbs';
+import { AuthorizationPage } from './pages/Authorization';
+import { RegistrationPage } from './pages/Registration';
+import { UserChangePasswordPage } from './pages/UserChangePassword';
+import { UserSettingsPage } from './pages/UserSettings';
 
-import Button from "./components/Button";
 
 
-// function render(html) {
+// function render(html:string) {
 //     const app = document.querySelector('#app');
 
 //     app.innerHTML = html;
 // }
 
 window.addEventListener('DOMContentLoaded', () => {
-    const root:HTMLElement|null = document.querySelector('#app');
-    // const context = {title: "Warm lamp chat"};
+    const root = document.querySelector('#app');
+    
+    // const auth = new AuthorizationPage();
+    // root.append(auth.getContent()!);
+    // auth.dispatchComponentDidMount();
+    
+    // const register = new RegistrationPage();
+    // root.append(register.getContent()!);
+    // register.dispatchComponentDidMount();
+
+
+    // const user_settings = new UserSettingsPage();
+    // root.append(user_settings.getContent()!);
+    // user_settings.dispatchComponentDidMount();
+
+    const user_change_password = new UserChangePasswordPage();
+    root.append(user_change_password.getContent()!);
+    user_change_password.dispatchComponentDidMount();
+    
+    // const context = {title: "Warm Lamp Chat"};
     // const html = template(context);
     // render(html);
 
-    const button = new Button({
-        label: "Click me",
-        events: {
-            click: () => console.log("Clicked!"),
-        }
-    });
+    // const button = new Button({
+    //     label: "Click me",
+    //     events: {
+    //         click: () => console.log("Clicked!"),
+    //     }
+    // });
 
-    root!.appendChild(button.element);
+    // const input = new Input({
+    //     type: "text",
+    //     name: "message",
+    //     placeholder: "Поиск",
+    //     events: {
+    //         click: () => console.log("Clicked!"),
+    //     }
+    // });
+
+    // root!.appendChild(button.element!);
+    // root!.appendChild(input.element!);
 });
---
+++ src/utils/renderDOM.ts
@@ -0,0 +1,17 @@
+// const ROUTES = {
+    
+// }
+
+
+// export function renderDOM(route: keyof typeof ROUTES) {
+//     const root = document.querySelector('#app');
+
+//     root.innerHTML = '';
+
+//     const PageComponent = ROUTES[route];
+//     const page = new PageComponent({});
+
+//     root.appendChild(page.element);
+
+//     page.dispatchComponentDidMount();
+// }
