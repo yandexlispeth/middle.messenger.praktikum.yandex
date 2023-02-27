ed6d2539d44dc9540887a95ee727d41d6c4b5f19
yandexlispeth <malinamagic@yandex.ru>
2023-02-28 02:14:19 +0300

    Страница Registration

---
+++ src/pages/Registration/index.ts
@@ -0,0 +1,62 @@
+import { Form } from "../../components/Form/Form";
+import Navigation from "../../components/Navigation";
+import Block from "../../utils/Block";
+import template from "./registration.hbs";
+
+
+export class RegistrationPage extends Block {
+    init() {
+        this.children.formRegister = new Form({
+            fields: [
+                {
+                type: "text",
+                name:"email",
+                placeholder: "Почта"
+            },
+            {
+                type: "text",
+                name: "login",
+                placeholder: "Пароль"
+            },
+            {
+                type: "text",
+                name: "first_name",
+                placeholder: "Имя"
+            },
+            {
+                type: "text",
+                name: "second_name",
+                placeholder: "Фамилия"
+            },
+            {
+                type:"text",
+                name: "phone",
+                placeholder: "Телефон"
+            },
+            {
+                type:"password",
+                name: "password",
+                placeholder: "Пароль"
+            }
+            ],
+            buttons: {
+                label: "Зарегистрироваться"
+            }
+        });
+
+
+        this.children.navigation = new Navigation({
+            link1: '/',
+            link2:'///',
+            menu_title1: 'Вход',
+            menu_title2: 'Регистрация',
+            events: {
+                click: () => console.log("navigation")
+            }
+        });
+    }
+
+    render() {
+        return this.compile(template, this.props);
+    }
+}
