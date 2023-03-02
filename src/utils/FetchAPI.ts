e7f7429c7f0678ad2c3d7700d08679a2bb5ddf25
yandexlispeth <malinamagic@yandex.ru>
2023-03-03 02:40:38 +0300

    FetchAPI

--- src/utils/FetchAPI.ts
+++ src/utils/FetchAPI.ts
@@ -1,67 +1,83 @@
 
-// const METHODS = {
-// 	GET: 'GET',
-// 	PUT: 'PUT',
-// 	POST: 'POST',
-// 	DELETE: 'DELETE'
-// };
+const enum METHODS {
+	GET = 1,
+	PUT,
+	POST,
+	DELETE
+}
 
 
-// function queryStringify(data) {
-// 	const arr = [];
-// 	for (const [key, value] of Object.entries(data)) {
-// 		arr.push(`${key}=${value}`);
-// 	}
-// 	return '?' + arr.join('&');
-// }
+function queryStringify(data:unknown) {
+	const arr = [];
+	for (const [key, value] of Object.entries(data)) {
+		arr.push(`${key}=${value}`);
+	}
+	return '?' + arr.join('&');
+}
 
-// export class HTTPTransport {
-// 	get = (url:string, options = {}) => {
-// 		return this.request(url, { ...options, method: METHODS.GET }, options.timeout);
-// 	};
+function getMethodName(method:METHODS):string {
+    switch(method) {
+        case METHODS.GET: return "GET";
+        case METHODS.POST: return "POST";
+        case METHODS.PUT: return "PUT";
+        case METHODS.DELETE: return "DELETE"
+    }
+}
 
-// 	post = (url:string, options = {}) => {
-// 		return this.request(url, { ...options, method: METHODS.POST }, options.timeout);
-// 	}
+interface IOptions {
+    method: METHODS,
+    headers: {[key: string]: string},
+    data: any,
+    timeout: number
+}
 
-// 	put = (url:string, options = {}) => {
-// 		return this.request(url, { ...options, method: METHODS.PUT }, options.timeout);
-// 	}
+export class HTTPTransport {
+	get = (url:string, options:IOptions) => {
+		return this.request(url, { ...options, method: METHODS.GET }, options.timeout);
+	};
 
-// 	delete = (url:string, options = {}) => {
-// 		return this.request(url, { ...options, method: METHODS.DELETE }, options.timeout);
-// 	}
+	post = (url:string, options:IOptions) => {
+		return this.request(url, { ...options, method: METHODS.POST }, options.timeout);
+	}
 
-// 	request = (url, options, timeout = 5000) => {
-// 		const xhr = new XMLHttpRequest();
-// 		if(options.method === METHODS.GET && options.data) {
-// 			url += queryStringify(options.data);
-// 		}
+	put = (url:string, options:IOptions) => {
+		return this.request(url, { ...options, method: METHODS.PUT }, options.timeout);
+	}
 
-// 		xhr.timeout = timeout;
+	delete = (url:string, options:IOptions) => {
+		return this.request(url, { ...options, method: METHODS.DELETE }, options.timeout);
+	}
+
+	request = (url:string, options:IOptions, timeout = 5000) => {
+		const xhr = new XMLHttpRequest();
+		if(options.method === METHODS.GET && options.data) {
+			url += queryStringify(options.data);
+		}
+
+		xhr.timeout = timeout;
       
-//     if(options.headers) {
-//       for (const [key, value] of Object.entries(options.headers)) {
-// 		xhr.setRequestHeader(key, value);
-//       }
-//     }
+    if(options.headers) {
+      for (const [key, value] of Object.entries(options.headers)) {
+		xhr.setRequestHeader(key, value);
+      }
+    }
       
-// 		return new Promise((resolve, reject) => {
-// 			xhr.open(options.method, url);
+		return new Promise((resolve, reject) => {
+			xhr.open(getMethodName(options.method), url);
 
-// 			xhr.onload = function () {
-// 				resolve(xhr);
-// 			};
+			xhr.onload = function () {
+				resolve(xhr);
+			};
 
-// 			xhr.onabort = reject;
-// 			xhr.onerror = reject;
-// 			xhr.ontimeout = reject;
+			xhr.onabort = reject;
+			xhr.onerror = reject;
+			xhr.ontimeout = reject;
 
-// 			if (options.method === METHODS.GET || !options.data) {
-// 				xhr.send();
-// 			} else {
-// 				xhr.send(options.data);
-//             }
-// 		});
-// 	};
-// }
+			if (options.method === METHODS.GET || !options.data) {
+				xhr.send();
+			} else {
+				xhr.send(options.data);
+            }
+		});
+	};
+}
